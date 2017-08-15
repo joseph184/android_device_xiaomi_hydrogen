a87a68ed94294368cc0bb7e74150d1b5a2ba917e
Dave <david-lang@outlook.de>
2017-08-15 06:38:16 +0200

    Update to XenonHD: Adapt files to XenonHD

--- xenonhd.mk
+++ xenonhd.mk
@@ -17,14 +17,14 @@
 
 $(call inherit-product, device/xiaomi/hydrogen/full_hydrogen.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common xenonhd stuff.
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := hydrogen
-PRODUCT_NAME := lineage_hydrogen
+PRODUCT_NAME := xenonhd_hydrogen
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi Max
 TARGET_VENDOR := Xiaomi
@@ -32,7 +32,14 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Enable root
-WITH_SU := true
+ROOT_METHOD=su
+
+PRODUCT_PACKAGES += \
+AdAway \
+MiXplorer \
+KernelAdiutor
+
+PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer=Dave
 
 # Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
